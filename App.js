8bef5c3d2e5f4b4da0520ea109653c1859834f16
Admin <khongcotinhban@gmail.com>
2022-09-28 02:57:32 +0700

    second commit

--- App.js
+++ App.js
@@ -1,33 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,ImageBackground, Text, View, Image } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet,ImageBackground, Text, View, Image, TextInput,TouchableOpacity,Alert } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(1);
+  // const onPresstru = () => setCount(prevCount=>prevCount-1);
+  const[dongia] = useState(141800);
+  const[thanhtien,settt] = useState(dongia)
+  
+  const [Tamtinh,setTamtinh] = useState(dongia);
+  const onPress = () => {setCount(count+1);setTamtinh(tach((count+1)*dongia));settt(tach((count+1)*dongia))};
+  const onPresstru = () => {
+    if(count>0)
+    {
+      setCount(count-1);
+      setTamtinh(tach((count-1)*dongia));
+      settt(tach((count-1)*dongia))
+    }
+    
+  };
   return (
     <View style={styles.main}>
       <View style={styles.view1}>
-        <Image style={styles.tinyLogo}
+        <Image
           source={require('./img/book.png')}
         />
-        <View>
+        <View style={[{height:140,marginLeft:15}]}>
           <Text style={styles.text}>Nguyên hàm phân tích và ứng dụng </Text> 
-          <Text style={styles.text1}>Cung cấp bởi Tiki Tranding</Text>
-          <Text style={styles.text2}>141.800đ</Text>
-          <Text style={styles.text3}>141.800đ</Text>
+          <Text style={[styles.text,{marginTop: 9}]}>Cung cấp bởi Tiki Tranding</Text>
+          <Text style={[styles.text,{marginTop: 9,color: 'red'}]}>{dongia}đ</Text>
+          <Text style={[styles.text,{marginTop: 9,color: '#808080'}]}>141.800đ</Text>
           <View style={styles.view2}>
-            <Image 
-              source={require('./img/btnminus.png')}
-            />
-            <Text style={styles.text6}>1</Text>
-            <Image
-            source={require('./img/btnadd.png')}
-            />
-            <Text style={styles.text4} >Mua sau</Text>
+            <TouchableOpacity onPress={onPresstru}>
+            <Image source={require('./img/btnminus.png')}></Image>
+            </TouchableOpacity>
+            <Text>{count}</Text>
+            
+            <TouchableOpacity onPress={onPress}>
+            <Image source={require('./img/btnadd.png')}></Image>
+            </TouchableOpacity>
+              <Text style={{fontWeight:'bold',color:'blue'}}>Mua sau</Text>
           </View>
         </View>
       </View>
       <View style={styles.view3}>
-      <Text style={styles.text5}>Mã giảm giá đã lưu</Text> 
+      <Text style={[{fontSize:13,fontWeight:'bold'}]}>Mã giảm giá đã lưu</Text> 
+      <Text style={[{marginLeft:18,fontSize:13,fontWeight:'bold',color:'blue'}]}>Xem  tại đây</Text>
       </View>
+      <View style={{paddingLeft:15,flexDirection:'row',marginTop:30,height:60}}>
+          <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+          <View style={styles.sectionStyle}>
+          <Image style={styles.imageStyle} source={require('./img/yellow_block.png')}></Image>
+          <TextInput
+            style={{flex: 1, fontSize:20}}
+            placeholder="Mã giảm giá"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+          <TouchableOpacity style={styles.button} >
+            <Text style={{fontSize:18,color:'white',fontWeight:'bold'}}>Áp dụng</Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{backgroundColor:'#808080',height:20,marginTop:20}}>
+        </View>
+        <View style={[styles.view3,{marginTop:20}]}>
+      <Text style={[{fontSize:13,fontWeight:'bold'}]}>Bạn có phiếu quà tặng Tiki/Got it/ Urbox?</Text> 
+      <Text style={[{marginLeft:18,fontSize:13,fontWeight:'bold',color:'blue'}]}>Nhập tại đây?</Text>
+      </View>
+      <View style={{backgroundColor:'#808080',height:20,marginTop:20}}>
+        </View>
+        <View style={[styles.view4,{marginTop:20}]}>
+      <Text style={[{fontSize:18,fontWeight:'bold'}]}>Tạm tính</Text> 
+      <Text style={[{marginLeft:18,fontSize:18,fontWeight:'bold',color:'red'}]}>{Tamtinh} đ</Text>
+      </View>
+      <View style={{backgroundColor:'#808080',height:140,marginTop:20}}>
+        </View>
+        <View style={[styles.view4,{marginTop:20}]}>
+      <Text style={[{fontSize:18,fontWeight:'bold',color:'#808080'}]}>Thành tiền</Text> 
+      <Text style={[{marginLeft:18,fontSize:18,fontWeight:'bold',color:'red'}]}>{thanhtien} đ</Text>
+      </View>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+          <TouchableOpacity style={styles.btnmua}>
+            <Text style={{fontSize:22,color:'white',fontWeight:'bold'}}>Tiến hành đặt hàng</Text>
+            </TouchableOpacity>
+          </View>
     </View>
   );
 }
@@ -38,63 +95,91 @@ const styles = StyleSheet.create({
   },
   view1: {
     marginTop:30,
-    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: 350,
-  },
-  tinyLogo: {
-    marginLeft: 20,
-    width: 100,
+    height: 150,
+    marginLeft:15
   },
   text: {
     fontSize: 13,
     fontWeight: "bold",
     textAlign: 'left',
   },
-  text1: {
-    fontSize: 13,
-    fontWeight: "bold",
-    textAlign: 'left',
-    marginTop: 7,
-  },
-  text2: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: 'left',
-    marginTop: 7,
-    color: 'red',
-  },
-  text3: {
-    fontSize: 13,
-    fontWeight: "bold",
-    textAlign: 'left',
-    marginTop: 7,
-    color: '#808080',
-  },
-  text4: {
-    fontSize: 13,
-    fontWeight: "bold",
-    color: 'blue',
-  },
-  text5: {
-    fontSize: 13,
-    fontWeight: "bold",
-    textAlign: 'left',
-    marginBottom: 570,
-    marginLeft: 18,
-  },
-  text6: {
-    fontWeight: "bold",
-  },
   view2: {
-    marginTop: 10,
+    marginTop: 13,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  view2: {
-    marginTop: 10,
+  view3: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    marginLeft:15
+  },
+  view4: {
+    flexDirection: 'row',
+    marginLeft:15,
+    marginRight:40,
+    justifyContent: 'space-between'
+  },
+  sectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#000',
+    height: 50,
+    borderRadius: 5,
+    
+    width:220
+  },
+  imageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 50,
+    resizeMode: 'stretch',
+    alignItems: 'center',
+  },
+  button:{
+    alignItems: "center",
+    justifyContent:'center',
+    height:50,
+    width:120,
+    backgroundColor: "#0A5EB7",
+    borderRadius:3,
+    marginLeft: 20
+
+  },
+  btnmua:{
+    alignItems: "center",
+    justifyContent:'center',
+    height:60,
+    width:400,
+    backgroundColor: "#E53935",
+    borderRadius:5
   },
 });
+
+const tach=(input)=>{
+
+  if(input<1000)
+  {
+    return input;
+  }
+  else if(input>=1000&&input<1000000){
+    ;
+    var a = parseInt(input/1000);
+    var b = input%1000;
+    if(b==0)return a+".000"
+    return a+"."+b;
+  }
+  else{
+    return input;
+  }
+  // else if(input>=100000&&input<100000000)
+  // {
+  //   var a = input/1000;
+  //   var b = input%1000;
+  //   var c
+  //   return a+"."+b
+  // }
+};
